75963158078707d505bdc1e5386b15874780909d
Aleksandar Beker <acabeker@gmail.com>
2021-12-04 12:59:50 +0100

    elasticsearch template is added

--- src/main/java/com/practice/demo/dto/ReaderDto.java
+++ src/main/java/com/practice/demo/dto/ReaderDto.java
@@ -1,8 +1,10 @@
 package com.practice.demo.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ReaderDto {
 
     private String firstName;
--- src/main/java/com/practice/demo/service/implementation/ReaderService.java
+++ src/main/java/com/practice/demo/service/implementation/ReaderService.java
@@ -1,18 +1,32 @@
 package com.practice.demo.service.implementation;
 
 import com.practice.demo.dto.ReaderDto;
+import com.practice.demo.dto.SimpleQueryEs;
+import com.practice.demo.dto.mapper.ReaderMapper;
 import com.practice.demo.model.Reader;
 import com.practice.demo.repository.IReaderRepository;
 import com.practice.demo.service.interfaces.IReaderService;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReaderService implements IReaderService {
 
     private final IReaderRepository _readerRepository;
+    private final ElasticsearchRestTemplate _elasticsearchRestTemplate;
 
-    public ReaderService(IReaderRepository readerRepository) {
+    public ReaderService(IReaderRepository readerRepository, ElasticsearchRestTemplate elasticsearchRestTemplate) {
         _readerRepository = readerRepository;
+        _elasticsearchRestTemplate = elasticsearchRestTemplate;
     }
 
     @Override
@@ -25,5 +39,26 @@ public class ReaderService implements IReaderService {
                     readerDto.getAddress()
             )
         );
+    }
+
+    @Override
+    public List<ReaderDto> readersByFirstNameLastName(String firstName, String lastName) {
+        QueryBuilder firstNameQuery = SearchQueryGenerator.createMatchQueryBuilder(new SimpleQueryEs("firstName", firstName));
+        QueryBuilder lastNameQuery = SearchQueryGenerator.createMatchQueryBuilder(new SimpleQueryEs("lastName", lastName));
+
+        BoolQueryBuilder boolQueryFirstNameAndLastName = QueryBuilders
+                .boolQuery()
+                .must(firstNameQuery)
+                .must(lastNameQuery);
+
+        return ReaderMapper.mapDtos(searchByBoolQuery(boolQueryFirstNameAndLastName));
+    }
+
+    private SearchHits<Reader> searchByBoolQuery(BoolQueryBuilder boolQueryBuilder) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(boolQueryBuilder)
+                .build();
+
+        return _elasticsearchRestTemplate.search(searchQuery, Reader.class,  IndexCoordinates.of("readers"));
     }
 }
